ca352ab6c9d267ba0df50948fc5330e80d1260d2
nimely111 <samuelehnimely@gmail.com>
2022-04-25 19:19:33 +0000

    refresh page to load a blurring image bacckgrund

--- script.js
+++ script.js
@@ -13,8 +13,11 @@ function blurring(){
     }
 
     loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bgCover.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 
 }
+
 
 https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 function scale (number, inMin, inMax, outMin, outMax) {
