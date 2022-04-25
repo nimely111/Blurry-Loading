b4be47465dd74f53f63e6bbb8ca5e764e081bc9a
nimely111 <samuelehnimely@gmail.com>
2022-04-25 18:38:12 +0000

    added the accending order for loading process

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const leadText = documentquerySelector('.loading-text');
-const bgCover = documentquerySelector('.bg-cover');
+const loadText = document.querySelector('.loading-text');
+const bgCover = document.querySelector('.bg-cover');
+
+let load = 0;
+let int = setInterval(blurring, 30);
 
 
+function blurring(){
+    load++;
 
+    if(load > 99){
+        clearInterval(int);
+    }
+
+    loadText.innerText = `${load}%`
+}
