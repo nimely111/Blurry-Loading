33494fc2cba9d3f5f4445a295d5ec02533170858
nimely111 <samuelehnimely@gmail.com>
2022-04-25 18:48:22 +0000

    added a stackover flow function for mapping a range of numbers to  another range of number

--- script.js
+++ script.js
@@ -12,5 +12,11 @@ function blurring(){
         clearInterval(int);
     }
 
-    loadText.innerText = `${load}%`
+    loadText.innerText = `${load}%`;
+
+}
+
+https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
